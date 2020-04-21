bb84ad4614cc91b927ffb85dc582624452644dba
Paolo Bianchini <paolo_bianchini@it.ibm.com>
2020-04-21 22:43:20 +0200

    removed controller user-credentials

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,4 +3,3 @@ export * from './event.controller';
 export * from './course.controller';
 export * from './score.controller';
 export * from './user.controller';
-export * from './user-user-credentials.controller';
